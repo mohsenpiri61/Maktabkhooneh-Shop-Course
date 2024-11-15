16a68df2caa333701eb50fc1b50389e918820f87
mopiry <mopiry@gmai.com>
2024-11-15 18:19:01 +0330

    coupon

--- core/dashboard/admin/forms/coupons.py
+++ core/dashboard/admin/forms/coupons.py
@@ -17,6 +17,13 @@ class CouponForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        instance = kwargs.get('instance')  
+        # print(vars(instance)) //note that print(instance) gives coupon's code because def __str__() was defined 'return self.code'
+
+        if instance and instance.used_by.exists():  # اگر کد تخفیف توسط کاربران استفاده شده باشد ، فیلدهای کد و درصد غیرفعال می شوند.
+            self.fields['code'].disabled = True
+            self.fields['discount_percent'].disabled = True
+        
         self.fields['code'].widget.attrs['class'] = 'form-control'
         self.fields['discount_percent'].widget.attrs['class'] = 'form-control'
         self.fields['max_limit_usage'].widget.attrs['class'] = 'form-control'

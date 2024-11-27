3f729ad11d61383dc98cd67c153bfe11a8f2c576
mopiry <mopiry@gmai.com>
2024-11-28 01:04:29 +0330

    verify

--- core/payment/views.py
+++ core/payment/views.py
@@ -10,18 +10,23 @@ from order.models import OrderModel, OrderStatusType
 
 class PaymentVerifyView(View):
     def get(self, request, *args, **kwargs):
-        authority_id = request.GET.get("authority")
-        status = request.GET.get("code")
+        authority_id = request.GET.get("Authority")
+        status = request.GET.get("Status")
 
-        payment_obj = get_object_or_404(
-            PaymentModel, authority_id=authority)
-        order = OrderModel.objects.get(payment=payment_obj)
+        # بازیابی اطلاعات پرداخت
+        payment_obj = get_object_or_404(PaymentModel, authority_id=authority_id)
+        
+        # مقداردهی زرین‌پال
         zarin_pal = ZarinPalSandbox() 
-        response = zarin_pal.payment_verify(
-            int(payment_obj.amount), payment_obj.authority_id)
-        ref_id = response["ref_id"]
-        status_code = response["code"]
-
+        response = zarin_pal.payment_verify(int(payment_obj.amount), payment_obj.authority_id)
+        
+        # استخراج داده‌های پاسخ
+        data = response.get("data", {})
+        errors = response.get("errors", [])
+        status_code = data.get("code", -1)
+        ref_id = data.get("ref_id")
+        
+        # ثبت اطلاعات پرداخت در مدل
         payment_obj.ref_id = ref_id
         payment_obj.response_code = status_code
         payment_obj.status = PaymentStatusType.success.value if status_code in {
@@ -29,8 +34,10 @@ class PaymentVerifyView(View):
         payment_obj.response_json = response
         payment_obj.save()
 
-        order.status = OrderStatusType.success.value if status_code in {
-            100, 101} else OrderStatusType.failed.value
+        # بروزرسانی وضعیت سفارش مرتبط
+        order = OrderModel.objects.get(payment=payment_obj)
+        order.status = OrderStatusType.PAID.value if status_code in {
+            100, 101} else OrderStatusType.CANCELED.value
         order.save()
-
+        
         return redirect(reverse_lazy("order:completed") if status_code in {100, 101} else reverse_lazy("order:failed"))
